760fc9efa055d643cbcef20d2512e38015afc114
unknown <trinhquangkhai2010@gmail.com>
2025-06-03 11:22:13 +0700

    update lab 03

--- lab-01/ex02/ex02_02.py
+++
@@ -1,6 +0,0 @@
-#Nhập bán kính từ người dùng
-ban_Kinh = float(input("Nhập bán kính của hình tròn: "))
-# Tinh diện tích của hình tròn
-dien_tich = 3.14 * (ban_Kinh ** 2)
-# Tính diện tích của hình tròn ra màn hình
-print("Diện tích của hình tròn là: ", dien_tich)
